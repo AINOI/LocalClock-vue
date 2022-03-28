2801d131cea15a40f3d0dafeb4393612b7947e0c
AINOI <ltccb30274@gmail.com>
2022-03-29 00:53:25 +0800

    update html title 02 0329

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path"
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    createHtmlPlugin({
+      minify: false,
+      inject: {
+        data: {
+          title: 'Local Clock',
+          injectScript: `<script src="./inject.js"></script>`
+        }
+      }
+    })
+  ],
   base: './',
   resolve: {
     alias: { '@/': `${path.resolve(__dirname, 'src')}/` }
