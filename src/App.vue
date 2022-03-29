4626f39d8ddbaa6ccec294c27249fa05f2225b18
AINOI <ltccb30274@gmail.com>
2022-03-29 16:36:09 +0800

    vue3 update 0329

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,38 @@
-<script>
-const date = new Date();
-export default {
-  name: "App",
-  data() {
-    return {
-      dateTime: {
-        hours: date.getHours(),
-        minutes: date.getMinutes(),
-        seconds: date.getSeconds(),
-      },
-      timer: undefined,
-    };
-  },
-  methods: {
-    setDateTime() {
-      const date = new Date();
-      this.dateTime = {
-        hours: date.getHours(),
-        minutes: date.getMinutes(),
-        seconds: date.getSeconds(),
-      };
-    },
-  },
-  beforeMount() {
-    this.timer = setInterval(this.setDateTime, 1000);
-  },
-  beforeUnmount() {
-    clearInterval(this.timer);
-  },
-};
+<script setup>
+import { onUnmounted, ref } from 'vue';
+
+const hours = ref('00')
+const mins = ref('00')
+const secs = ref('00')
+
+const setDateTime = setInterval(() => {
+  const date = ref(new Date())
+  const hourTimer = ref(date.value.getHours())
+  const minTimer = ref(date.value.getMinutes())
+  const secTimer = ref(date.value.getSeconds())
+  
+  hours.value = hourTimer.value
+  mins.value = minTimer.value
+  secs.value = secTimer.value
+
+  hours.value = hourTimer.value < 10 ? "0" + hourTimer.value : hourTimer.value
+  mins.value = minTimer.value < 10 ? "0" + minTimer.value : minTimer.value
+  secs.value = secTimer.value < 10 ? "0" + secTimer.value : secTimer.value
+}, 1000)
+
+onUnmounted(() => clearInterval(setDateTime()))
+
 </script>
 
 <template lang="pug">
 link(href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet")
 .clock-box
   .clock
-    .hour {{ dateTime.hours }}
+    .hour {{ hours }}
     .dot :
-    .min {{ dateTime.minutes }}
+    .min {{ mins }}
     .dot :
-    .sec {{ dateTime.seconds }}
+    .sec {{ secs }}
   .circle
     .waterwave
 </template>
